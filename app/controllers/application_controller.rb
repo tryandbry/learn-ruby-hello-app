a51656d6fa8d86c40693ee4b1c9eaf830f07cbbc
bryan <bryan@localhost.localdomain>
2017-09-29 08:54:16 -0500

    completes up to 1.3 in railstutorial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡hola, mundo!"
+  end
+
+  def goodbye
+    render html: 'goodbye, world!'
+  end
 end
